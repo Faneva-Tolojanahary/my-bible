0617ad63d527cb723c2778c9c3df1c9e9a4df481
Faneva-Tolojanahary <fanevarb560@gmail.com>
2026-04-25 20:34:39 +0200

    Modif index for cors

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require("express");
 require("./src/config/database");
 const bookRoutes = require("./src/routes/book.router");
+const cors = require("cors");
 
 const app = express();
+
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+  allowedHeaders: ['Content-Type', 'Authorization'],
+  credentials: true 
+}));
 
 app.use("/api", bookRoutes);
 
 
-
-app.listen(3000, () => {
+app.listen(5000, () => {
     console.log("Serveur lancé");
 });
